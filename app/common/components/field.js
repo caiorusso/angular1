82a693946902e3d6230d00e1bf4a074182be6580
Caio Russo <caiorusso@gmail.com>
2017-05-12 13:32:00 -0300

    Alterações no Controller, adição do método de exclusão

--- app/common/components/field.js
+++ app/common/components/field.js
@@ -8,6 +8,7 @@
       type: '@',
       //binding bidirecional
       model: '=',
+      readonly: '<',
     },
     controller: [
       'gridSystem',
@@ -20,7 +21,7 @@
       <div class="form-group">
         <label for="{{ $ctrl.id }}" >{{ $ctrl.label }}</label>
         <input id="{{ $ctrl.id}}" class="form-control" placeholder="{{ $ctrl.placeholder }}"
-        type="{{ $ctrl.type }}" ng-model="$ctrl.model" />
+        type="{{ $ctrl.type }}" ng-model="$ctrl.model" ng-readonly="$ctrl.readonly"/>
       </div>
     </div>
     `
