2767245fc517e09f00c10e26b16014023c2822cb
Caio Russo <caiorusso@gmail.com>
2017-05-12 11:26:58 -0300

    Factory de tabs, consumindo a API nos métodos criar e listar

---
+++ app/billingCycle/billingCycleController.js
@@ -0,0 +1,32 @@
+(function() {
+    angular.module('firstApp').controller('BillingCycleCtrl', [
+      '$http',
+      'msgs',
+      'tabs',
+      BillingCycleController
+    ])
+
+    function BillingCycleController($http, msgs, tabs) {
+      const vm = this
+      const url = 'http://localhost:3003/api/billingcycles'
+
+      vm.refresh = function() {
+        $http.get(url).then(function(response) {
+          vm.billingCycle = {}
+          vm.billingcycles = response.data
+          tabs.show(vm, {tabList: true, tabCreate: true})
+        })
+      }
+
+      vm.create = function() {
+        $http.post(url, vm.billingCycle).then(function(response) {
+          vm.refresh()
+          msgs.addSuccess('Operação realizada com sucesso')
+        }).catch(function(response) {
+          msgs.addError(response.data.errors)
+        })
+      }
+
+      vm.refresh()
+    }
+})()
